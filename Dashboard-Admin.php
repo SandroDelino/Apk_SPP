34fe67f5fd2f20ca04a110454398f666bb86ae20
SandroDelino <114328681+SandroDelino@users.noreply.github.com>
2023-02-10 18:01:33 +0700

    Add files via upload

---
+++ Dashboard-Admin.php
@@ -0,0 +1,21 @@
+<?php
+include "../Tamplate-SPP/Header.php";
+include "../Tamplate-SPP/Navbar.php";
+include "../Tamplate-SPP/Sidebar.php";
+?>
+<div class="main-content">
+  <section class="section">
+    <div class="row">
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <h4>Dashboard Admin</h4>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+<?php include "../Tamplate-SPP/Footer.php"; ?>
---
+++ Sidebar.php
@@ -0,0 +1,21 @@
+<div class="main-sidebar sidebar-style-2">
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a>PEMBAYARAN SPP</a>
+</div>
+<div class="sidebar-brand sidebar-brand-sm">
+
+</div>
+<ul class="sidebar-menu">
+    <li><a class="nav-link" href="Dashboard-Admin.php"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+    <li><a class="nav-link" href="Crud-Petugas.php"><i class="fas fa-id-card"></i><span>CRUD Data Petugas</span></a></li>
+    <li><a class="nav-link" href="Crud-Siswa.php"><i class="fas fa-id-card"></i><span>CRUD Data Siswa</span></a></li>
+    <li><a class="nav-link" href="Crud-Kelas.php"><i class="fas fa-id-card"></i><span>CRUD Data Kelas</span></a></li>
+    <li><a class="nav-link" href="Crud-SPP.php"><i class="fas fa-file-invoice-dollar"></i><span>CRUD Data SPP</span></a></li>
+    <li><a class="nav-link" href="Entri-Transaksi.php"><i class="fas fa-dollar-sign"></i><span>Entri Transaksi</span></a></li>
+    <li><a class="nav-link" href="History.php"><i class="fas fa-history"></i><span>History Pembayaran</span></a></li>
+    <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <i class="fas fa-rocket"></i> Logout
+</a>
+</div>                </aside>
+</div>
